e141d0341e824f8a9c12b4997b6975a89e5903b9
AdibRahim <adib.rahim07@gmail.com>
2022-11-07 14:50:30 +0800

    Init Projek

--- routes/web/_.php
+++ routes/web/_.php
@@ -52,4 +52,8 @@ Route::post('/email/verification-notification', function (Request $request) {
     return back()->with('message', 'Verification link sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
+Route::get('/crud',function(){
+    return 'test';
+});
+
 LaravelMediaSecure::routes();
