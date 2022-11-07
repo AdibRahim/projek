3de6a9d0ad8d15e0095145bbc139ed124e178501
AdibRahim <AdibRahim@users.noreply.github.com>
2022-11-07 08:15:20 +0000

    PHP Linting (Pint)

--- routes/web/_.php
+++ routes/web/_.php
@@ -52,7 +52,7 @@ Route::post('/email/verification-notification', function (Request $request) {
     return back()->with('message', 'Verification link sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
-Route::get('/crud',function(){
+Route::get('/crud', function () {
     return 'test';
 });
 
